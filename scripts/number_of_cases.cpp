cd731ff24091e30cc4be8174ca13e6502adc9a4c
root <root@suzuranPC.localdomain>
2025-12-26 00:56:44 +0900

    add number_of_cases

---
+++ scripts/number_of_cases.cpp
@@ -0,0 +1,40 @@
+#include <iostream> 
+#include <cmath> 
+#include <vector> 
+#include <queue> 
+#include <string> 
+#include <sstream> 
+#include <algorithm>  
+#include <limits>  
+#include <climits>  
+#include <tuple>  
+#include <set>  
+
+#define M_PI 3.14159265358979323846
+
+using namespace std;
+using ll = long long;
+
+ll facctorialMethod(int n) {
+	ll result = 1;
+	for (int i = 2; i <= n; i++) {
+		result *= i;
+	}
+	return result;
+}
+
+ll permutationMethod(int n, int r) {
+	// nPr‚ðŒvŽZ
+	ll nNum = n - r;
+	ll result = 1;
+	for (int i = n; i > nNum; i--) {
+		result *= i;
+	}
+	return result;
+}
+
+ll combinationMethod(int n, int r) {
+	// nCr‚ðŒvŽZ
+	ll result = permutationMethod(n, r) / facctorialMethod(r);
+	return result;
+}
